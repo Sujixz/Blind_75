6ec4941ac0f9a214590e47f4ec03c8980d47b531
Sujal Chavan <sujalchavan035@gmail.com>
2026-02-06 20:11:19 +0530

    Buy and Sell Stock |Sliding window| O(n)

---
+++ Sliding Window/Buy_and_sell_stock.py
@@ -0,0 +1,10 @@
+#  Best time to Buy and Sell Stock
+class Solution(object):
+    def maxProfit(self, prices):
+        maxP = 0
+        minBuy = prices[0]
+
+        for sell in prices:
+            maxP = max(maxP ,sell - minBuy)
+            minBuy = min(minBuy, sell)
+        return maxP    
